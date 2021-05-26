351d03c5e88a4e8d3377e29b4392d342184cad22
daltoncoder <daltoncoder2@gmail.com>
2021-05-26 19:34:55 -0400

    working frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-
-function App() {
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+const App = () => {
   return (
-    <>
-      <h1>Welcome To the Shop</h1>
-    </>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
