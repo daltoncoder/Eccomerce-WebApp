58b2c1b2cd7fc91c7cf2e84aa33eee8c73205632
daltoncoder <daltoncoder2@gmail.com>
2021-05-27 06:44:19 -0400

    added local env variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
-
 const products = require('./data/products')
+
+dotenv.config()
 
 app.get('/', (req, res) => {
   res.send('Api is working')
@@ -16,4 +18,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
